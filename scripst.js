61d79d1f49b2e2532588d90f9c313fc0d08f64d4
dg121111 <dfgaldamesg@gmail.com>
2025-07-13 18:26:09 -0400

    Update scripst.js

--- scripst.js
+++ scripst.js
@@ -27,64 +27,84 @@
       { id: "organica", nombre: "Química Orgánica", prereq: ["quim2"] },
       { id: "intro", nombre: "Introducción a la Ingeniería Genética", prereq: [] },
       { id: "Acui", nombre: "Acuicultura", prereq: [] },
-      { id: "mate4", nombre: "Métodos y Aplicaciones de las Ecuaciones Diferenciales", prereq: ["mate3"] },
+      { id: "mate4", nombre: "Ecuaciones Diferenciales", prereq: ["mate3"] },
       { id: "bioq", nombre: "Bioquímica", prereq: ["bio1", "organica"] },
       { id: "termo", nombre: "Termodinámica", prereq: ["quim2", "optica", "mate2"] },
-      { id: "ingles1", nombre: "Inglés Cientifico I", prereq: [] },
-      { id: "CFG1", nombre: "Curso Formación General I", prereq: [] },
+      { id: "ingles1", nombre: "Inglés Científico I", prereq: [] },
+      { id: "CFG1", nombre: "CFG I", prereq: [] },
       { id: "gene", nombre: "Genética", prereq: ["bioq"] },
       { id: "microbio", nombre: "Microbiología", prereq: ["bioq"] },
       { id: "bioesta", nombre: "Bioestadística", prereq: ["mate1"] },
       { id: "cine", nombre: "Cinética y Electroquímica", prereq: ["termo", "mate4"] },
       { id: "bio_mol", nombre: "Biología Molecular", prereq: ["microbio", "gene"] },
       { id: "fisio", nombre: "Fisioquímica Macromolecular", prereq: ["bioq", "cine"] },
-      { id: "tallerge", nombre: "Taller de Ingeniería Genética", prereq: ["gene"] },
-      { id: "elec", nombre: "Electivo Especialidad o Unidad de Investigación Electiva", prereq: [] },
-      { id: "CFG2", nombre: "Curso Formación General II", prereq: [] },
+      { id: "tallerge", nombre: "Taller Ingeniería Genética", prereq: ["gene"] },
+      { id: "elec", nombre: "Electivo Especialidad", prereq: [] },
+      { id: "CFG2", nombre: "CFG II", prereq: [] },
       { id: "biotec", nombre: "Biotecnología", prereq: ["bio_mol"] },
       { id: "instru", nombre: "Instrumentación", prereq: ["fisio"] },
       { id: "fisioge", nombre: "Fisiología General", prereq: ["bioq"] },
-      { id: "eco", nombre: "Ecología Microbiana y Biotecnología Ambiental", prereq: ["gene"] },
-      { id: "ingles2", nombre: "Inglés Cientifico II", prereq: ["ingles1"] },
+      { id: "eco", nombre: "Ecología Microbiana", prereq: ["gene"] },
       { id: "inmuno", nombre: "Inmunología", prereq: ["bio_mol"] },
       { id: "microeco", nombre: "Microeconomía", prereq: ["mate2"] },
       { id: "fisioveg", nombre: "Fisiología Vegetal", prereq: ["bio_mol"] },
       { id: "opti", nombre: "Optimización", prereq: ["mate4"] },
+      { id: "ingles2", nombre: "Inglés Científico II", prereq: ["ingles1"] },
       { id: "bioet", nombre: "Bioética", prereq: ["gene"] },
-      { id: "patente", nombre: "Patentes y Legislación", prereq: ["tallerge"] },
       { id: "microin", nombre: "Microbiología Industrial", prereq: ["microbio"] },
       { id: "biotecmed", nombre: "Biotecnología Médica", prereq: ["bio_mol"] },
       { id: "opera", nombre: "Operaciones Unitarias", prereq: ["opti"] },
-      { id: "tallerbio", nombre: "Taller de Biotecnología Vegetal", prereq: ["fisioveg"] },
-      { id: "Uni", nombre: "Unidad de Investigación de Seminario de Título", prereq: [] },
+      { id: "tallerbio", nombre: "Taller Biotecnología Vegetal", prereq: ["fisioveg"] },
+      { id: "patente", nombre: "Patentes y Legislación", prereq: ["tallerge"] },
+      { id: "Uni", nombre: "Unidad de Investigación", prereq: [] },
       { id: "eva", nombre: "Evaluación de Proyectos", prereq: [] },
       { id: "semi", nombre: "Seminario de Título", prereq: [] },
     ];
 
-    const container = document.getElementById("malla");
+    const container = document.getElementById("contenedor-malla");
 
-    ramosPorSemestre.forEach((semestre, i) => {
-      const cont = document.createElement("div");
-      cont.className = "semestre";
+    for (let i = 0; i < ramosPorSemestre.length; i += 2) {
+      const anio = document.createElement("div");
+      anio.className = "anio";
+
       const titulo = document.createElement("div");
-      titulo.className = "titulo-semestre";
-      titulo.textContent = `${i + 1}º Semestre`;
-      cont.appendChild(titulo);
+      titulo.className = "anio-titulo";
+      titulo.textContent = `${i / 2 + 1}º Año`;
+      anio.appendChild(titulo);
 
-      semestre.forEach(id => {
-        const ramo = ramos.find(r => r.id === id);
-        const div = document.createElement("div");
-        div.className = "ramo";
-        div.id = ramo.id;
-        div.innerText = ramo.nombre;
-        if (ramo.prereq.length === 0) {
-          div.classList.add("activo");
-          div.onclick = () => toggleAprobado(div, ramo.id);
+      const fila = document.createElement("div");
+      fila.className = "malla";
+
+      for (let j = 0; j < 2; j++) {
+        const idx = i + j;
+        if (ramosPorSemestre[idx]) {
+          const cont = document.createElement("div");
+          cont.className = "semestre";
+
+          const tituloSem = document.createElement("div");
+          tituloSem.className = "titulo-semestre";
+          tituloSem.textContent = `${idx + 1}º Semestre`;
+          cont.appendChild(tituloSem);
+
+          ramosPorSemestre[idx].forEach(id => {
+            const ramo = ramos.find(r => r.id === id);
+            const div = document.createElement("div");
+            div.className = "ramo";
+            div.id = ramo.id;
+            div.innerText = ramo.nombre;
+            if (ramo.prereq.length === 0) {
+              div.classList.add("activo");
+              div.onclick = () => toggleAprobado(div, ramo.id);
+            }
+            cont.appendChild(div);
+          });
+          fila.appendChild(cont);
         }
-        cont.appendChild(div);
-      });
-      container.appendChild(cont);
-    });
+      }
+
+      anio.appendChild(fila);
+      container.appendChild(anio);
+    }
 
     function toggleAprobado(elemento, id) {
       elemento.classList.toggle("aprobado");
@@ -109,5 +129,6 @@
     }
   </script>
 </body>
+</html>
 
 
