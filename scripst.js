e90237bd29e0ce899ead3b6ff4fcaaf16554be5f
dg121111 <dfgaldamesg@gmail.com>
2025-07-13 17:36:45 -0400

    Update scripst.js

--- scripst.js
+++ scripst.js
@@ -1,2 +1,18 @@
+// script.js
+const ramos = [
+  { nombre: "Biología I", semestre: 1, descripcion: "Ramo introductorio" },
+  { nombre: "Química General", semestre: 1, descripcion: "Fundamentos de química" },
+  { nombre: "Anatomía", semestre: 2, descripcion: "Estudio del cuerpo humano" },
+];
+
+const container = document.getElementById("malla-container");
+
+ramos.forEach(ramo => {
+  const div = document.createElement("div");
+  div.className = "ramo";
+  div.innerText = ramo.nombre;
+  div.title = ramo.descripcion;
+  container.appendChild(div);
+});
 
 
